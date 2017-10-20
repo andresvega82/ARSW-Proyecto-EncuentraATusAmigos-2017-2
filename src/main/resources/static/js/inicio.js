d9fb4625daf5ac5c4d06abc019777c17ca88d9f6
jairo1094 <2090540@linux22.labinfo.is.escuelaing.edu.co>
2017-10-20 13:43:37 -0500

    javaScript por HTML

---
+++ src/main/resources/static/js/inicio.js
@@ -0,0 +1,18 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var inicioJs = (function () {
+    
+    return{
+        tableFriends:function(nameUs){
+            
+        },
+        insertName: function(nombreMan){
+            document.getElementById("nameUser").innerHTML = nombreMan;
+        }
+    }
+    
+})();
